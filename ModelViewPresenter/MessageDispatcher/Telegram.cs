0d3cac3ee55528519d569771a0df19234f039053
Enrico <enrico@zilverband.com>
2015-12-02 17:45:55 +0800

    *Added Operation enumerator

--- ModelViewPresenter/MessageDispatcher/Telegram.cs
+++ ModelViewPresenter/MessageDispatcher/Telegram.cs
@@ -6,16 +6,24 @@ using System.Threading.Tasks;
 
 namespace ModelViewPresenter.MessageDispatcher
 {
+    public enum Operation
+    {
+        None = 0,
+        OpenView = 1
+    }
+
     public struct Telegram
     {
         public int _sender;
         public int _receiver;
         public dynamic _data;
+        public Operation _operation;
 
-        public Telegram(int sender, int receiver, dynamic data)
+        public Telegram(int sender, int receiver, Operation operation, dynamic data)
         {
             this._sender = sender;
             this._receiver = receiver;
+            this._operation = operation;
             this._data = data;
         }
     }
